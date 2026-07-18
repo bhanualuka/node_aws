4c0ca8491e5b5a82fe72da65d4d54d5d02053557
bhanu aluka <alukabhanuprakash@gmail.com>
2026-07-18 12:53:21 +0530

    debug

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+console.log("Application started - Version 2");
+
 app.get("/get", (req, res) => {
   res.json({ message: "jai sai master jai bapuji maharaj" });
 });
 
 app.get("/hello", (req, res) => {
+  console.log("Hello route called");
   res.json({ message: "Jai Bapuji maharaj , modified Code" });
 });
 
